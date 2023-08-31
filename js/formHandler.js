1333325e07447e2c0af279cd6ad9b6a6d59b6f40
i.pashin <i.pashin@softmg.ru>
2023-08-31 10:20:56 +0300

    Update

---
+++ js/formHandler.js
@@ -0,0 +1,84 @@
+document.querySelector('#leave-request-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const description = document.querySelector('#request-description')
+    const name = document.querySelector('#request-name')
+    const contacts = document.querySelector('#contacts')
+    const phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+    const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+    if (!description.value) {
+        description.style.outline = "rgb(255, 77, 87) solid 2px"
+        description.addEventListener('input', () => {
+            description.style.outline = 'none'
+        })
+    } else {
+        if (!name.value) {
+            name.style.outline = "rgb(255, 77, 87) solid 2px"
+            name.addEventListener('input', () => {
+                name.style.outline = 'none'
+            })
+        } else {
+            if (!contacts.value || !(phoneRegex.test(contacts.value) || emailRegex.test(contacts.value))) {
+                contacts.style.outline = "rgb(255, 77, 87) solid 2px"
+                contacts.addEventListener('input', () => {
+                    contacts.style.outline = 'none'
+                })
+            } else {
+                const data = {
+                    name: name.value,
+                    description: description.value,
+                    contacts: contacts.value
+                }
+                handleEmail(data)
+                document.querySelectorAll('input:not(#contractor-email, input[type="submit"]), textarea').forEach((el) => { 
+                    el.value = null
+                    el.checked = false
+                })
+            }
+        }
+    }
+})
+document.querySelector('#request-offer').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const name = document.querySelector('#name-request')
+    const contacts = document.querySelector('#contacts-request')
+    const phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+    const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+    if (!name.value) {
+        name.style.outline = "rgb(255, 77, 87) solid 2px"
+        name.addEventListener('input', () => {
+            name.style.outline = 'none'
+        })
+        document.querySelector('.offer-error-name').innerHTML = 'Неправильная форма'
+    } else {
+        document.querySelector('.offer-error-name').innerHTML = '&nbsp;'
+        if (!contacts.value || !(phoneRegex.test(contacts.value) || emailRegex.test(contacts.value))) {
+            contacts.style.outline = "rgb(255, 77, 87) solid 2px"
+            contacts.addEventListener('input', () => {
+                contacts.style.outline = 'none'
+            })
+            document.querySelector('.offer-error-contacts').innerHTML = 'Неправильная форма'
+        } else {
+            document.querySelector('.offer-error-contacts').innerHTML = '&nbsp;'
+            const data = {
+                name: name.value,
+                contacts: contacts.value
+            }
+            document.querySelectorAll('#request-offer input:not(input[type="submit"])').forEach((el) => { el.value = null })
+            document.querySelector('.offer-request-wrapper').classList.add('hidden')
+            handleEmail(data)
+        }
+    }
+})
+document.querySelector('.contractor-input').addEventListener('submit', (e) => {
+    e.preventDefault()
+    document.querySelector('.contractor-input input[type="email"]').value = null
+    handleEmail([])
+})
+
+function handleEmail(data) {
+    document.querySelector('.sendmail-popup-wrapper').classList.toggle('hidden')
+}
---
+++ js/main.js
@@ -0,0 +1,159 @@
+swiperEl = document.querySelector('.slider-banner-content')
+
+
+
+
+advantEl = document.querySelector('.advant-slider')
+
+const paramsAdvant = {
+  shouldPreventDefault: true,
+  navigation: {
+    prevEl: '#prevAdvant',
+    nextEl: '#nextAdvant'
+  },
+  slideToClickedSlide: true,
+  breakpoints: {
+    0: {
+      spaceBetween: 16,
+      slidesPerView: 'auto'
+    },
+    484: {
+      spaceBetween: 24,
+      slidesPerView: 'auto'
+    },
+    1280: {
+      spaceBetween: 36,
+      slidesPerView: 'auto'
+    }
+  }
+}
+
+Object.assign(advantEl, paramsAdvant)
+advantEl.initialize()
+
+
+const workSlider = new Swiper('.work-slider', {
+  slidesPerView: 1,
+  effect: 'fade',
+  fadeEffect: { crossFade: true },
+  autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+  },
+  pagination: {
+      el: '.work-pagination',
+      renderBullet: (index, className) => {
+          index = index + 1;
+          return '<span class="' + className + '"><span class="stage">Этап ' + index + '</span></span>'
+      },
+      clickable: true
+  }
+})
+
+function toggleAccordeon(el) {
+  el.classList.toggle('active')
+  var panel = el.querySelector('.accordeon-subject')
+  if (panel.style.maxHeight) {
+    panel.style.maxHeight = null
+  } else {
+    panel.style.maxHeight = panel.scrollHeight + 'px'
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tab-head')
+  tabs.forEach((el) => {
+    el.addEventListener('click', () => {
+      tabs.forEach((tab) => {
+        tab.classList.remove('active')
+      })
+      el.classList.add('active')
+      const content = document.querySelectorAll('.tab-content')
+      content.forEach((con) => {
+        con.classList.remove('active')
+      })
+      content[el.getAttribute('data-id')].classList.add('active') 
+    })
+  })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  const stage = document.querySelectorAll('.work-section')
+  let activeIndex = 1
+  const stageLength = stage.length 
+  setInterval(() => {
+    stage[activeIndex % stageLength].classList.add('active')
+    stage[(activeIndex - 1) % stageLength].classList.remove('active')
+    activeIndex++
+  }, 5000)
+})
+
+function updateFiles() {
+  filelist = '<div class="selected-files">'
+  Array.from(document.querySelector('#file').files).forEach((file, index) => {
+    filename = file.name > 21 ? file.name.substr(0, 21) : file.name
+    filelist += `<span class="selected-file">${filename}<img class="remove-file" data-id="${index}" src="./images/filecross.svg"></span>`
+  })
+  document.querySelector('.file-desc').innerHTML = filelist + '</div>'
+  
+  document.querySelectorAll('.remove-file').forEach((el) => {
+    el.addEventListener('click', (e) => {
+      e.preventDefault()
+      removeFileFromFileList(Number(el.getAttribute('data-id')))
+    })
+  })
+}
+
+document.querySelector('#file').addEventListener('change', updateFiles)
+
+document.querySelector('.menu-trigger').addEventListener('click', () => {
+  const wrapper = document.querySelector('.slide-menu-wrapper')
+  wrapper.classList.toggle('active')
+  if (wrapper.classList.contains('active')) {
+    document.querySelector('.menu-trigger img').setAttribute('src', 'images/big_cross.svg')
+  } else {
+    document.querySelector('.menu-trigger img').setAttribute('src', 'images/burger.svg')
+  }
+})
+
+document.querySelector('.slide-menu .menu-items').addEventListener('click', () => {
+  document.querySelector('.slide-menu-wrapper').classList.remove('active')
+  document.querySelector('.menu-trigger img').setAttribute('src', 'images/burger.svg')
+})
+
+document.querySelector('.main-nav.mobile .logo').addEventListener('click', () => {
+  document.querySelector('.slide-menu-wrapper').classList.remove('active')
+  document.querySelector('.menu-trigger img').setAttribute('src', 'images/burger.svg')
+})
+
+document.querySelector('.slide-menu-wrapper .header-button').addEventListener('click', () => {
+  document.querySelector('.slide-menu-wrapper').classList.toggle('active')
+  document.querySelector('.menu-trigger img').setAttribute('src', 'images/burger.svg')
+})
+
+
+function removeFileFromFileList(index) {
+  const dt = new DataTransfer()
+  const input = document.querySelector('#file')
+  const { files } = input
+  
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i]
+    if (index !== i)
+      dt.items.add(file)
+  }
+  
+  updateFiles()
+  input.files = dt.files
+}
+
+document.querySelectorAll('.close-popup').forEach((el) => {
+  el.addEventListener('click', () => {
+    document.querySelector('.sendmail-popup-wrapper').classList.add('hidden')
+    document.querySelector('.offer-request-wrapper').classList.add('hidden')
+  })
+})
+
+function openRequest(el) {
+  document.querySelector('.offer-request-wrapper').classList.remove('hidden')
+}
