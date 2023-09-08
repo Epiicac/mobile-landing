89210700dac48222315e5b82956eab75d144670d
i.pashin <i.pashin@softmg.ru>
2023-09-08 16:04:31 +0300

    Drafts before test 6

--- js/main.js
+++ js/main.js
@@ -50,16 +50,6 @@ const workSlider = new Swiper('.work-slider', {
   }
 })
 
-function toggleAccordeon(el) {
-  el.classList.toggle('active')
-  var panel = el.querySelector('.accordeon-subject')
-  if (panel.style.maxHeight) {
-    panel.style.maxHeight = null
-  } else {
-    panel.style.maxHeight = panel.scrollHeight + 'px'
-  }
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   const tabs = document.querySelectorAll('.tab-head')
   tabs.forEach((el) => {
@@ -88,11 +78,67 @@ document.addEventListener('DOMContentLoaded', () => {
   }, 5000)
 })
 
+function toggleAccordeon(el) {
+  el.classList.toggle('active')
+  var panel = el.querySelector('.accordeon-subject')
+  if (panel.style.maxHeight) {
+    panel.style.maxHeight = null
+  } else {
+    panel.style.maxHeight = panel.scrollHeight + 'px'
+  }
+}
+
+var files = []
+document.addEventListener('DOMContentLoaded', () => {
+    $('#file').on('change', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        for (let file of event.target.files) {
+            if (notDuplicate(files, file)) {
+                files.push(file)
+            }
+        }
+        data = new DataTransfer()
+        for (let file of files) {
+            data.items.add(file)
+        }
+        event.target.files = data.files
+        updateFiles()
+    });
+})
+function removeFileFromFileList(index) {
+    files.splice(index, 1)
+    data = new DataTransfer()
+    for (let file of files) {
+        data.items.add(file)
+    }
+    document.getElementById('file').files = data.files
+    updateFiles()
+}
+ 
 function updateFiles() {
+  fileArray = document.querySelector('#file').files
   filelist = '<div class="selected-files">'
-  Array.from(document.querySelector('#file').files).forEach((file, index) => {
-    filename = file.name > 21 ? file.name.substr(0, 21) : file.name
-    filelist += `<span class="selected-file">${filename}<img class="remove-file" data-id="${index}" src="./images/filecross.svg"></span>`
+  document.querySelector('.count-error').style.display = 'none'
+  document.querySelector('.size-error').style.display = 'none'
+  fileSizeLimit = 20971520
+  cumulativeFileSize = 0
+  Array.from(fileArray).forEach((file, index) => {
+    cumulativeFileSize += file.size
+    filename = file.name.length > 21 ? file.name.substr(0, 21) + '...' : file.name
+    if (cumulativeFileSize > fileSizeLimit) {
+      if (file.size > fileSizeLimit) {
+        filelist += `<span class="selected-file error">${filename}<img class="remove-file" data-id="${index}" src="./images/filecross.svg"></span>`
+      } else {
+        filelist += `<span class="selected-file">${filename}<img class="remove-file" data-id="${index}" src="./images/filecross.svg"></span>`
+      }
+      fileSizeError()
+    } else {
+      filelist += `<span class="selected-file">${filename}<img class="remove-file" data-id="${index}" src="./images/filecross.svg"></span>`
+    }
+    if (index > 9) {
+      fileLimitError()
+    }
   })
   document.querySelector('.file-desc').innerHTML = filelist + '</div>'
   
@@ -100,11 +146,28 @@ function updateFiles() {
     el.addEventListener('click', (e) => {
       e.preventDefault()
       removeFileFromFileList(Number(el.getAttribute('data-id')))
+      updateFiles()
     })
   })
+
+  var select_null = document.querySelector('.selected-files')
+  if (select_null.innerHTML == '') {
+    select_null.style.height = "auto"
+    select_null.style.padding = "0px"
+    select_null.style.overflow = "auto"
+    select_null.innerHTML = '<span class="file-hint">Прикрепить файлы</span><span class="file-hint-desc">Загружаемые файлы не должны превышать 20 мб</span>'
+  }
+}
+
+function fileLimitError() {
+  document.querySelector('.count-error').style.display = 'block'
+}
+function fileSizeError() {
+  document.querySelector('.size-error').style.display = 'block'
 }
 
 document.querySelector('#file').addEventListener('change', updateFiles)
+
 
 document.querySelector('.menu-trigger').addEventListener('click', () => {
   const wrapper = document.querySelector('.slide-menu-wrapper')
@@ -130,22 +193,6 @@ document.querySelector('.slide-menu-wrapper .header-button').addEventListener('c
   document.querySelector('.slide-menu-wrapper').classList.toggle('active')
   document.querySelector('.menu-trigger img').setAttribute('src', 'images/burger.svg')
 })
-
-
-function removeFileFromFileList(index) {
-  const dt = new DataTransfer()
-  const input = document.querySelector('#file')
-  const { files } = input
-  
-  for (let i = 0; i < files.length; i++) {
-    const file = files[i]
-    if (index !== i)
-      dt.items.add(file)
-  }
-  
-  updateFiles()
-  input.files = dt.files
-}
 
 document.querySelectorAll('.close-popup').forEach((el) => {
   el.addEventListener('click', () => {
